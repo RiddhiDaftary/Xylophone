3f6cf16354b52c6c619ce7eaba527ec61d654f68
RiddhiDaftary <rkdaftary@gmail.com>
2020-09-19 15:04:23 +0530

    xylophone app for playing sound using 3rd party libraries and dart funcitons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(Color colorOfKey, int soundNumber, String soundName){
+    return Expanded(
+      child: MaterialButton(
+        child: Text(
+          '$soundName',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+          ),
+        ),
+        color: colorOfKey,
+        onPressed: (){
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red,1,"Sa"),
+              buildKey(Colors.orange,2,"Re"),
+              buildKey(Colors.yellow,3,"Ga"),
+              buildKey(Colors.green,4,"Ma"),
+              buildKey(Colors.teal,5,"Pa"),
+              buildKey(Colors.blue,6,"Dha"),
+              buildKey(Colors.purple,7,"Ni"),
+            ],
+          )
         ),
       ),
     );
